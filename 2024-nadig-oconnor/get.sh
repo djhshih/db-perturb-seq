c07a21f2e21686d668ecf3d12fc39500d1213be2
David J. H. Shih <djh.shih@gmail.com>
2025-08-18 22:04:22 +0800

    Add raw data download.

--- 2024-nadig-oconnor/get.sh
+++ 2024-nadig-oconnor/get.sh
@@ -6,3 +6,7 @@ IFS=$'\n\t'
 
 wget -cL https://ftp.ncbi.nlm.nih.gov/geo/series/GSE264nnn/GSE264667/suppl/GSE264667_hepg2_raw_singlecell_01.h5ad
 wget -cL https://ftp.ncbi.nlm.nih.gov/geo/series/GSE264nnn/GSE264667/suppl/GSE264667_jurkat_raw_singlecell_01.h5ad
+
+wget -cL https://ftp.ncbi.nlm.nih.gov/geo/series/GSE264nnn/GSE264667/suppl/GSE264667_RAW.tar
+tar -xf GSE264667_RAW.tar
+rm GSE264667_RAW.tar
