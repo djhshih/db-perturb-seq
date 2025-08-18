4bf4460cf5508f97b1d815e21a94c2734ea9e490
David J. H. Shih <djh.shih@gmail.com>
2025-08-18 18:39:54 +0800

    Add GSE264667

---
+++ 2024-nadig-oconnor/get.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Download dataset
+
+set -euo pipefail
+IFS=$'\n\t'
+
+wget -cL https://ftp.ncbi.nlm.nih.gov/geo/series/GSE264nnn/GSE264667/suppl/GSE264667_hepg2_raw_singlecell_01.h5ad
+wget -cL https://ftp.ncbi.nlm.nih.gov/geo/series/GSE264nnn/GSE264667/suppl/GSE264667_jurkat_raw_singlecell_01.h5ad
